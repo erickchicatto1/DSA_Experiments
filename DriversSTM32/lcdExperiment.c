ddda0502b209a0a5a0bc5bdb6c4fa450fc3bbe59
erickchicatto1 <e57072271@gmail.com>
2024-10-25 21:15:02 -0600

    Adding more stuffs in order to can detect the serial com
    adding more logic

--- DriversSTM32/lcdExperiment.c
+++ DriversSTM32/lcdExperiment.c
@@ -1,3 +1,6 @@
+//To do : Understand the logic , debug
+//To do :
+
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
@@ -21,6 +24,11 @@ UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 
+uint8_t Buffer[25] = {0};
+uint8_t Space[] = " - ";
+uint8_t StartMSG[] = "Starting I2C Scanning: \r\n";
+uint8_t EndMSG[] = "Done! \r\n\r\n";
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -42,21 +50,35 @@ void lcd_backlight(uint8_t state);
 int main(void)
 {
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+  uint8_t i = 0;
+  uint8_t ret;
+  char *text = "EmbeddedThere";
+  char int_to_str[10];
+  int count=0;
 
-  /* Configure the system clock */
+  HAL_Init();
   SystemClock_Config();
 
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
   lcd_init();
   lcd_backlight(1); // Turn on backlight
 
-  char *text = "EmbeddedThere";
-  char int_to_str[10];
-  int count=0;
+  HAL_UART_Transmit(&huart2,StartMSG,sizeof(StartMSG),10000);
+  for(i=1;i<128;i++){
+	  ret = HAL_I2C_IsDeviceReady(&hi2c1,(uint16_t)(i<<1),3,5);
+
+	  if(ret!=HAL_OK){ /* No ACK Received At That Address */
+		  HAL_UART_Transmit(&huart2, Space, sizeof(Space), 10000);
+	  }
+	  else if(ret == HAL_OK){
+		  sprintf(Buffer, "0x%X", i);
+		  HAL_UART_Transmit(&huart2, Buffer, sizeof(Buffer), 10000);
+	  }
+	  HAL_UART_Transmit(&huart2, EndMSG, sizeof(EndMSG), 10000);
+	 /*--[ Scanning Done ]--*/
+  }
 
   while (1)
   {
@@ -69,7 +91,6 @@ int main(void)
 	  count++;
 	  memset(int_to_str, 0, sizeof(int_to_str));
 	  HAL_Delay(1500);
-
   }
 }
 
@@ -81,15 +102,8 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -104,11 +118,8 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+                           |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -120,21 +131,10 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
+
 static void MX_I2C1_Init(void)
 {
 
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
@@ -148,27 +148,11 @@ static void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
-/**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+
 static void MX_USART2_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -181,17 +165,8 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
 }
 
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -221,6 +196,8 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+/*Accomodate the bites in order to send the I2C*/
+
 void lcd_write_nibble(uint8_t nibble, uint8_t rs) {
   uint8_t data = nibble << D4_BIT;
   data |= rs << RS_BIT;
@@ -300,13 +277,6 @@ void lcd_backlight(uint8_t state) {
   }
 }
 
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
